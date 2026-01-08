55d66633a29c3f3ea1903dc15fe0011e275644f0
kaiwu <khoavu1831@gmail.com>
2026-01-08 21:45:34 +0700

    feat: add prisma - orm

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,48 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import { authRoute } from "./routes/authRoute.js"
+import { prisma } from "./libs/prisma.js"
 
-dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const user = await prisma.user.create({
+  data: {
+    username: "khoa",
+    email: "khoa@test.com",
+    hash_password: "hashed_password",
+    first_name: "Khoa",
+    last_name: "Nguyen"
+  }
+});
 
+
+console.log("Tao thanh cong" + user);
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// routes
+// private route
+app.use('/api/auth', authRoute);
+
+// public route
+
+
 app.get("/", (req, res) => {
   res.json({ message: "Xin chao" });
+});
+
+app.get("/test-db", async (req, res) => {
+  const user = await prisma.user.findMany();
+  res.json(user);
+});
+
+
+// run
+app.listen(PORT, async () => {
+  console.log(`Server đang chạy cổng: ${PORT}`);
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-})
